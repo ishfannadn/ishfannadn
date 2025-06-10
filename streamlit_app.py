75c1ed0afa3b3cd16f78fb5c28bfa2f63b52267c
ishfannadn <ishfannadinara@gmail.com>
2025-06-10 19:22:05 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,5 @@ import streamlit as st
 st.title("🎈 Latihan membuat Website")
 st.write(
     "Latihan membuat website melalui Github dan Steamlit dengan menggunakan bahasa Pyhton"
-
 )
 st.image("296E7974-DF4E-4F88-945C-E393F941D2FE.png", width=200)

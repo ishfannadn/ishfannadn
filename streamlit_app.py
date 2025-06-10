b4a08448a52b2ed41398a865a70afe6b31af671b
ishfannadn <ishfannadinara@gmail.com>
2025-06-10 18:28:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ st.write(
     "Latihan membuat website melalui Github dan Steamlit dengan menggunakan bahasa Pyhton"
 
 )
-st.image("https://github.com/ishfannadn/ishfannadn/blob/main/296E7974-DF4E-4F88-945C-E393F941D2FE.png", width=200)
+st.image("296E7974-DF4E-4F88-945C-E393F941D2FE.png", width=200)
